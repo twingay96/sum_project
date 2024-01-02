c8eb0794234d5814b2a5ad509156e2551cdbcefe
twingay96 <flavonoid37@gmail.com>
2024-01-02 19:41:52 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'static_public#landing_page'
+  get 'privacy', to: 'static_public#privacy'
+  get 'terms', to: 'static_public#privacy'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
