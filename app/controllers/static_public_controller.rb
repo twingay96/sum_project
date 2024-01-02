567b4a05e5764dc3b87009c81cd8397a16378161
twingay96 <flavonoid37@gmail.com>
2024-01-02 23:26:55 +0900

    devise gem,nav-bar, footer추가

--- app/controllers/static_public_controller.rb
+++ app/controllers/static_public_controller.rb
@@ -1,4 +1,6 @@
 class StaticPublicController < ApplicationController
+  before_action :authenticate_user!, except: [:landing_page]
+  #skip_before_action :authenticate_user!, only: %i[:landing_page]
   def landing_page
   end
 
