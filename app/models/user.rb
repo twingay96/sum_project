b019906790fb250c8477c36cadd825abfdf2009e
twingay96 <flavonoid37@gmail.com>
2024-01-07 21:31:35 +0900

    email,provider,image,name컬럼추가 및 user.rb 메소드 수정

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,21 +6,17 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
-    #user = User.where(email: data['email']).first_or_initialize
+    user = User.where(email: access_token.info.email).first
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
       user = User.create(
-        email: data['email'],
+        email: access_token.info.email,
+        provider: access_token.provider,
+        name: access_token.info.name,
+        image: access_token.info.image,
         password: Devise.friendly_token[0,20]
       )
     end
-    # if user.new_record?
-    #   user.name = data['name']
-    #   user.password = Devise.friendly_token[0,20]
-    #   user.save! # 유효성 검사에 실패하면 예외가 발생합니다.
-    # end
     user
   end
 end
--- db/migrate/20240106182403_add_name_to_users.rb
+++ db/migrate/20240106182403_add_name_to_users.rb
@@ -1,5 +1,4 @@
 class AddNameToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :name, :string
   end
 end
