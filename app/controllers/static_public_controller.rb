4e0078abf1267fd293a3ba7451933c0821626029
twingay96 <flavonoid37@gmail.com>
2024-01-11 22:58:46 +0900

    _messages의 flash동작수정

--- app/controllers/static_public_controller.rb
+++ app/controllers/static_public_controller.rb
@@ -2,6 +2,11 @@ class StaticPublicController < ApplicationController
   before_action :authenticate_user!, except: [:landing_page]
   #skip_before_action :authenticate_user!, only: %i[:landing_page]
   def landing_page
+    unless session[:visit_landing_page]
+      flash.now[:visit_first]="this is landingpage!"
+      session[:visit_landing_page]= true
+      
+    end
   end
 
   def privacy
