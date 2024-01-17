fee431c10b0673cdc1c03f64cf135cb913936d87
twingay96 <flavonoid37@gmail.com>
2024-01-17 23:46:22 +0900

    _user.html 수정

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,16 +1,16 @@
-Ransack.configure do |config|
+# Ransack.configure do |config|
 
-    # Change default search parameter key name.
-    # Default key name is :q
-    config.search_key = :query
+#     # Change default search parameter key name.
+#     # Default key name is :q
+#     config.search_key = :query
   
-    # Raise errors if a query contains an unknown predicate or attribute.
-    # Default is true (do not raise error on unknown conditions).
-    config.ignore_unknown_conditions = false
+#     # Raise errors if a query contains an unknown predicate or attribute.
+#     # Default is true (do not raise error on unknown conditions).
+#     config.ignore_unknown_conditions = false
   
-    # Globally display sort links without the order indicator arrow.
-    # Default is false (sort order indicators are displayed).
-    # This can also be configured individually in each sort link (see the README).
-    config.hide_sort_order_indicators = true
+#     # Globally display sort links without the order indicator arrow.
+#     # Default is false (sort order indicators are displayed).
+#     # This can also be configured individually in each sort link (see the README).
+#     config.hide_sort_order_indicators = true
   
-  end
+#   end
