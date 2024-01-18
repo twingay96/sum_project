0f528046653e21b2491088262c0444bb2d09e214
twingay96 <flavonoid37@gmail.com>
2024-01-18 23:54:10 +0900

    viewhelper추가

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,36 @@
 module ApplicationHelper
     include Pagy::Frontend
+
+    def active_link_to(name,path)
+        content_tag(:li , class: "#{'active fw-bold' if current_page?(path)} nav-item ") do
+            link_to name, path, class: "nav-link" 
+        end
+    end
+
+    def deep_active_link_to(path)
+        content_tag(:li , class: "#{'active fw-bold' if current_page?(path)} nav-item ") do
+            link_to path, class: "nav-link" do
+                yield
+            end
+        end
+    end
+
+    def deep_active_link_to_dropdown_item(path)
+        content_tag(:li) do
+            link_to path ,class: "#{'active fw-bold' if current_page?(path)} dropdown-item" do
+                yield
+            end
+        end
+    end
+
+    def boolean_label(value)
+        case value
+        when true
+            badge_color = "badge bg-sucess"
+        when false
+            badge_color = "badge bg-danger"
+        end
+        content_tag(:span, value, class: badge_color)
+    end
+
 end
